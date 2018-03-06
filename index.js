e15f145fac7b2c504af4f9efa73c53ea0d1ce531
Shaun Baer <shaun.baer@gmail.com>
2018-03-05 23:35:45 -0500

    Update partials

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Mkdirp = require('mkdirp');
 const Globby = require('globby');
 const GUtil = require('gulp-util');
 const Path = require('path');
+const fs = require('fs');
 
 
 /**
@@ -19,14 +20,17 @@ const echoDefaults = {
   // default layout
   layout: 'default',
 
+  // Directory where partials live
+  partialsDir: 'src/partials/',
+
   // common entries
-  common: 'src/partials/common/**/*',
+  common: 'common/**/*',
 
   // blocks
-  blocks: 'src/partials/blocks/**/*',
+  blocks: 'blocks/**/*',
 
   // helpers
-  helpers: 'src/partials/helpers/**/*',
+  helpers: 'helpers/**/*',
 
 };
 
@@ -110,162 +114,76 @@ const registerPartials = () => {
   // Make sure data is clear
   echoData.partials = {};
 
-  const allPartials = [echoOpts.common, echoOpts.blocks, echoOpts.helpers];
+  const allPartials = [
+    Path.normalize(echoOpts.partialsDir + echoOpts.common), 
+    Path.normalize(echoOpts.partialsDir + echoOpts.blocks), 
+    Path.normalize(echoOpts.partialsDir + echoOpts.helpers)
+  ];
 
   // Get all files that should be made into partials
   const files = Globby.sync(allPartials, {nodir: true, nosort: true});
 
-  // build a glob for identifying directories
-  const dirsGlob = allPartials.map(function (pattern) {
-    return Path.dirname(pattern) + '/*/';
-  });
 
-  // get all directories
-  // do a new glob; trailing slash matches only dirs
-  var dirs = Globby.sync(dirsGlob).map(function (dir) {
-    return Path.normalize(dir).split(Path.sep).slice(-2, -1)[0];
-  });
-
-  // Find and store all working directories
-  let workingDirs = [];
-
-  allPartials.forEach( (dir) => {
-    let dirArr = dir.split(Path.sep);
-
-    dirArr.forEach( (dirName) => {
-      if ( dirName == '\*' || dirName == '\*\*' || dirName == '') {
-        return;
-      } else {
-        workingDirs.push(dirName);
-      }
-    });
-  });
-  
-  workingDirs = _.uniq(workingDirs);
-
-  console.log(workingDirs);
-
-  let fileIDPath;
-
-  // console.log(dirs);
-
-  console.log('Files:');
-  console.log('----------');
-
-  // stub out an object for each collection and subCollection
   files.forEach(function (file) {
+    
+    
+    let stubs = Path.normalize(Path.dirname(file)).replace(echoOpts.partialsDir, '').split(Path.sep).slice(1);
+    
+    let parent = stubs.length > 1 ? stubs.slice(-2, -1)[0] : stubs[0];
+    parent = parent === undefined ? false : parent;
+    
+    let collection = parent ? stubs.pop() : false;
+    collection = collection === parent ? false : collection;
 
-    var parent = getName(Path.normalize(Path.dirname(file)).split(Path.sep).slice(-2, -1)[0], true);
-    var collection = getName(Path.normalize(Path.dirname(file)).split(Path.sep).pop(), true);
-    var isSubCollection = (dirs.indexOf(parent) > -1);
+    let id = collection ? collection + '__' + Path.basename(file, Path.extname(file)) : Path.basename(file, Path.extname(file));
+    
+    // console.log(file + ':');
+    // console.log('parent:     ' + parent);
+    // console.log('collection: ' + collection);
+    // console.log('id:         ' + id);
 
-    // get the material base dir for stubbing out the base object for each category (e.g. component, structure)
-    var materialBase = (isSubCollection) ? parent : collection;
-
-    // stub the base object
-    echoData.partials[materialBase] = echoData.partials[materialBase] || {
-      name: toTitleCase(getName(materialBase)),
-      items: {}
-    };
-
-    if (isSubCollection) {
-      echoData.partials[parent].items[collection] = echoData.partials[parent].items[collection] || {
-        name: toTitleCase(getName(collection)),
-        items: {}
-      };
-    }
-
-    // let type = getName(Path.normalize(Path.dirname(file)).split(Path.sep).slice(-2, -1)[0], true);
-    // var collection = getName(Path.normalize(Path.dirname(file)).split(Path.sep).pop(), true);
-    // var isSubCollection = (dirs.indexOf(parent) > -1);
-
-    // const splitPath = Path.normalize(Path.dirname(file)).split(Path.sep);
-    // const idPath = _.difference(splitPath, workingDirs);
-
-    // console.log(type);
-    // console.log(collection);
-    // console.log(workingDirs);
-    // console.log(splitPath);
-    // console.log(idPath);
-    // console.log(file);
-    // console.log('--');
-
-    // // get the material base dir for stubbing out the base object for each category (e.g. component, structure)
-    // var materialBase = (isSubCollection) ? parent : collection;
-
-    // // stub the base object
-    // assembly.materials[materialBase] = assembly.materials[materialBase] || {
-    //   name: toTitleCase(getName(materialBase)),
-    //   items: {}
-    // };
-
-    // if (isSubCollection) {
-    //   assembly.materials[parent].items[collection] = assembly.materials[parent].items[collection] || {
-    //     name: toTitleCase(getName(collection)),
-    //     items: {}
-    //   };
-    // }
-
-  });
-
-  console.log(echoData.partials);
-
-  
-  // iterate over each file (material)
-  files.forEach(function (file) {
 
     // get info
-    // var fileMatter = Matter(file);
-    // var collection = getName(Path.normalize(Path.dirname(file)).split(Path.sep).pop(), true);
-    // var parent = Path.normalize(Path.dirname(file)).split(Path.sep).slice(-2, -1)[0];
-    // var isSubCollection = (dirs.indexOf(parent) > -1);
-    // var id = (isSubCollection) ? getName(collection) + '.' + getName(file) : getName(file);
-    // var key = (isSubCollection) ? collection + '.' + getName(file, true) : getName(file, true);
+    var fileMatter = Matter.read(file);
 
-    // // get material front-matter, omit `notes`
-    // var localData = _.omit(fileMatter.data, 'notes');
-
-    // // trim whitespace from material content
-    // var content = fileMatter.content.replace(/^(\s*(\r?\n|\r))+|(\s*(\r?\n|\r))+$/g, '');
+    // trim whitespace from material content
+    var content = fileMatter.content.replace(/^(\s*(\r?\n|\r))+|(\s*(\r?\n|\r))+$/g, '');
+    
+    // get local file data in front matter
+    var localData = _.omit(fileMatter.data, 'notes');
 
 
-    // capture meta data for the material
-    // if (!isSubCollection) {
-    //   echoData.partials[collection].items[key] = {
-    //     name: toTitleCase(id),
-    //     notes: (fileMatter.data.notes) ? md.render(fileMatter.data.notes) : '',
-    //     data: localData
-    //   };
-    // } else {
-    //   echoData.partials[parent].items[collection].items[key] = {
-    //     name: toTitleCase(id.split('.')[1]),
-    //     notes: (fileMatter.data.notes) ? md.render(fileMatter.data.notes) : '',
-    //     data: localData
-    //   };
-    // }
-
-
-    // store material-name-spaced local data in template context
-    // assembly.materialData[id.replace(/\./g, '-')] = localData;
-
-
+    /**
+     * THIS REPLACE INVLOVES POINTING TO AN ID
+     * IF PARTIALS ARE A PROBLEM, LOOK HERE!!
+     * 
+     */
     // replace local fields on the fly with name-spaced keys
     // this allows partials to use local front-matter data
     // only affects the compilation environment
-    // if (!_.isEmpty(localData)) {
-    //   _.forEach(localData, function (val, key) {
-    //     // {{field}} => {{material-name.field}}
-    //     var regex = new RegExp('(\\{\\{[#\/]?)(\\s?' + key + '+?\\s?)(\\}\\})', 'g');
-    //     content = content.replace(regex, function (match, p1, p2, p3) {
-    //       return p1 + id.replace(/\./g, '-') + '.' + p2.replace(/\s/g, '') + p3;
-    //     });
-    //   });
-    // }
+    if (!_.isEmpty(localData)) {
+      _.forEach(localData, function (val, key) {
+        // {{field}} => {{material-name.field}}
+        var regex = new RegExp('(\\{\\{[#\/]?)(\\s?' + key + '+?\\s?)(\\}\\})', 'g');
+        content = content.replace(regex, function (match, p1, p2, p3) {
+          return p1 + id.replace(/\./g, '-') + '.' + p2.replace(/\s/g, '') + p3;
+        });
+      });
+    }
+
+
 
     // register the partial
-    // Handlebars.registerPartial(id, content);
+    Handlebars.registerPartial(id, content);
 
+    console.log('----------');
   });
+  
+
+  
+    
+
+  // });
   
 
 }
